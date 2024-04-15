b5cd84feffd8c95186df1fb1c7b7efe2ed002ffd
Jeremy Cope <jcope0787@protonmail.com>
2024-04-15 16:36:28 -0700

    added struct snake

--- headers/snake.h
+++ headers/snake.h
@@ -1,4 +1,11 @@
 #ifndef CSNAKE_SNAKESTRUCT_HEADER_H
 #define CSNAKE_SNAKESTRUCT_HEADER_H
 
+#include "../cDynamPointArr/dynamPointArr.h"
+
+struct snake {
+        struct dynamPointArr *body;
+        struct point head;
+};
+
 #endif /* CSNAKE_SNAKESTRUCT_HEADER_H */
