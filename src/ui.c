59e7c1e9a2b70a147a13c9fb3a6e51d047db4e7e
Jeremy Cope <jcope0787@protonmail.com>
2024-04-15 10:57:52 -0700

    added code to create border

--- src/ui.c
+++ src/ui.c
@@ -1,1 +1,15 @@
 #include "../headers/ui.h"
+
+void makeBorder()
+{
+        char borderChar = '+';
+        for (int iii = 0; iii <= 80; iii++) {
+                mvaddch(0, iii, borderChar);
+                mvaddch(24, iii, borderChar);
+        }
+        for (int iii = 1; iii <= 23; iii++) {
+                mvaddch(iii, 0, borderChar);
+                mvaddch(iii, 80, borderChar);
+        }
+}
+
