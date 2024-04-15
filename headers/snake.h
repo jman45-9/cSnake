91e11710498baa249caa0d94a9f70ef90c57e5e2
Jeremy Cope <jcope0787@protonmail.com>
2024-04-15 15:06:28 -0700

    Revert "added files for a dynamic point array lib"
    This reverts commit 97923ddeb1f40bfbb23ee8351f3ae3cfaaa3fb80.

--- headers/snake.h
+++ headers/snake.h
@@ -1,8 +1,4 @@
 #ifndef CSNAKE_SNAKESTRUCT_HEADER_H
 #define CSNAKE_SNAKESTRUCT_HEADER_H
 
-#include <ncurses.h>
-#include "../cDynamPointArr/dynamPointArr.h"
-
-
 #endif /* CSNAKE_SNAKESTRUCT_HEADER_H */
