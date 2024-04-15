97923ddeb1f40bfbb23ee8351f3ae3cfaaa3fb80
Jeremy Cope <jcope0787@protonmail.com>
2024-04-15 15:04:16 -0700

    added files for a dynamic point array lib

--- headers/snake.h
+++ headers/snake.h
@@ -1,4 +1,8 @@
 #ifndef CSNAKE_SNAKESTRUCT_HEADER_H
 #define CSNAKE_SNAKESTRUCT_HEADER_H
 
+#include <ncurses.h>
+#include "../cDynamPointArr/dynamPointArr.h"
+
+
 #endif /* CSNAKE_SNAKESTRUCT_HEADER_H */
